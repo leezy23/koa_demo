0e83b82efe506c4d7f3c2fbb6254bb5724c20703
lizhouyang <lizy@cnns.net>
2019-04-01 11:28:08 +0800

    整理代码

--- koa_demo/app.js
+++ koa_demo/app.js
@@ -1,27 +1,3 @@
-// const Koa = require('koa');
-// const app = new Koa();
-// const indent = (n) => new Array(n).join(' ');
-// const mid1 = () => async (ctx, next) => {
-//   ctx.body = `<h3>请求 => 第一层中间件</h3>`;
-//   console.log("==============================1");
-//   await next();
-//   ctx.body += `<h3>响应 <= 第一层中间件</h3>`;
-//   console.log("==============================2")
-// };
-// const mid2 = () => async (ctx, next) => {
-//   ctx.body += `<h3>${indent(4)}请求 => 第二层中间件</h3>`;
-//   console.log("==============================3");
-//   await next();
-//   ctx.body += `<h3>${indent(4)}响应 <= 第二层中间件</h3>`;
-//   console.log("==============================4")
-// };
-// app.use(mid1());
-// app.use(mid2());
-// app.use(async (ctx, next) => {
-//   console.log("==============================5");
-//   ctx.body += `<p style="color: #f60">${indent(12)}=> Koa 核心 处理业务 <=</p>`
-// });
-// app.listen(3333);
 const Koa = require('koa');
 const A = require('./routes/A');
 const B = require('./routes/B');
